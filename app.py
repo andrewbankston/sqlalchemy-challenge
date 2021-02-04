6ec1ab0bc98b1170a6dcf30a6bab00a8a1173713
andrewbankston <andrewbankston3@gmail.com>
2021-02-03 18:43:04 -0800

    created app routes for home, precipitation, stations, and temp

--- app.py
+++ app.py
@@ -12,7 +12,14 @@ import sys
 
 engine = create_engine("sqlite:///hawaii.sqlite", echo=False)
 
-app = Flast(__name__)
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+app = Flask(__name__)
 
 @app.route("/")
 def home():
@@ -26,8 +33,46 @@ def home():
         f"/api.v1.0/<start>/<end>"
     )
 
-@app.route("/api.v1.0/precipitation")
+@app.route("/api/v1.0/precipitation")
 def precip():
     session = Session(engine)
 
-    results = session.query()
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    session.close()
+
+    weather_meas = []
+
+    for date, prcp in results:
+        weather_dict = {}
+        weather_dict[date] = prcp
+        weather_meas.append(weather_dict)
+
+    return jsonify(weather_meas)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+
+    results = session.query(Station.station).all()
+
+    session.close()
+
+    station_list = list(np.ravel(results))
+
+    return jsonify(station_list)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date > '2016-08-24').filter(Measurement.station=='USC00519281').all()
+
+    session.close()
+
+    temp_list = list(np.ravel(results))
+
+    return jsonify(temp_list)
+
+if __name__ == '__main__':
+    app.run(debug=False)
